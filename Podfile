008b52361b44c322a80f3209d56a375de11626b3
Oktawian Chojnacki <oktawian@me.com>
2016-02-24 16:47:18 +0100

    Add Decodable.

--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ platform :tvos, '9.0'
 use_frameworks!
 
 pod 'Alamofire', '~> 3.0'
+pod 'Decodable', '~> 0.4'
---
+++ Pods/Target Support Files/Decodable/Decodable-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Decodable : NSObject
+@end
+@implementation PodsDummy_Decodable
+@end
