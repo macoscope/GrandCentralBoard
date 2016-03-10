26ee43604c4b5af2a2789231ae900853ac8ac255
Oktawian Chojnacki <oktawian@me.com>
2016-03-10 11:00:42 +0100

    Make GCBCore testable.

--- GrandCentralBoardTests/Core/GrandCentralBoardTests.swift
+++ GrandCentralBoardTests/Core/GrandCentralBoardTests.swift
@@ -5,7 +5,7 @@
 
 import XCTest
 import Decodable
-import GrandCentralBoardCore
+@testable import GrandCentralBoardCore
 @testable import GrandCentralBoard
 
 class GrandCentralBoardTests: XCTestCase {
