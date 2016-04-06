15dfb900c19d3781a7ba94b58fb49ccaf5190daa
Bartek Chlebek <bartek.public@gmail.com>
2016-04-06 16:30:23 +0200

    GCB-127: Code cleanup

--- GrandCentralBoard/Core/Extensions/UIView.swift
+++ GrandCentralBoard/Core/Extensions/UIView.swift
@@ -29,8 +29,7 @@ public extension UIView {
     }
 }
 
-private var isFlashingKey: UInt8 = 0
-
+/// () -> Void cannot be stored as associatedObject, we need a wrapper.
 private final class BlockWrapper {
     let block: () -> Void
     
@@ -40,50 +39,52 @@ private final class BlockWrapper {
 }
 
 public extension UIView {
+    private struct AssociatedKey {
+        static var onDidBecomeActiveKey = "onDidBecomeActiveKey"
+    }
     
-    private var onApplicationDidBecomeActive: BlockWrapper? {
+    private var onApplicationDidBecomeActive: (() -> Void)? {
         get {
-            return objc_getAssociatedObject(self, &isFlashingKey) as? BlockWrapper
+            let wrapper = objc_getAssociatedObject(self, &AssociatedKey.onDidBecomeActiveKey) as? BlockWrapper
+            return wrapper?.block
         }
         set {
             let notificationCenter = NSNotificationCenter.defaultCenter()
             let notificationName = UIApplicationDidBecomeActiveNotification
+            let selector = #selector(applicationDidBecomeActiveNotification)
             
             if newValue == nil {
                 notificationCenter.removeObserver(self, name: notificationName, object: nil)
             }
-            else {
-                let selector = #selector(applicationDidBecomeActiveNotification)
+            else if self.onApplicationDidBecomeActive == nil {
                 notificationCenter.addObserver(self, selector: selector, name: notificationName, object: nil)
             }
             
-            return objc_setAssociatedObject(self, &isFlashingKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return objc_setAssociatedObject(self,
+                                            &AssociatedKey.onDidBecomeActiveKey,
+                                            newValue.map(BlockWrapper.init),
+                                            .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
     
     @objc private func applicationDidBecomeActiveNotification() {
-        self.onApplicationDidBecomeActive?.block()
+        self.onApplicationDidBecomeActive?()
     }
     
     func startFlashingWithInterval(interval: NSTimeInterval, alphaDepth: CGFloat) {
         
-        self.onApplicationDidBecomeActive = BlockWrapper { [weak self] in
+        self.onApplicationDidBecomeActive = { [weak self] in
             self?.stopFlashing()
             self?.startFlashingWithInterval(interval, alphaDepth: alphaDepth)
         }
         
-        UIView.animateWithDuration(interval, delay: 0.0, options:
-            [
-                .CurveEaseInOut,
-                .Autoreverse,
-                .Repeat
-            ],
-            animations: {
-                self.alpha = alphaDepth
-            },
-            completion: nil)
+        UIView.animateWithDuration(interval,
+                                   delay: 0.0,
+                                   options: [.CurveEaseInOut, .Autoreverse, .Repeat],
+                                   animations: { self.alpha = alphaDepth },
+                                   completion: nil)
     }
-
+    
     func stopFlashing() {
         self.onApplicationDidBecomeActive = nil
         layer.removeAllAnimations()
