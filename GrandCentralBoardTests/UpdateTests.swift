def7a3ef329d721247a4764bae07255db3aa82a6
Bartek Chlebek <bartek.public@gmail.com>
2016-04-07 13:14:12 +0200

    GCB-129: Improved dates readability in tests

--- GrandCentralBoardTests/UpdateTests.swift
+++ GrandCentralBoardTests/UpdateTests.swift
@@ -10,6 +10,20 @@ import XCTest
 import Nimble
 @testable import GrandCentralBoard
 
+private let ISO8601DateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    return formatter
+}()
+
+private extension NSDate {
+    convenience init?(ISO8601String: String) {
+        guard let date = ISO8601DateFormatter.dateFromString(ISO8601String) else { return nil }
+        self.init(timeIntervalSince1970: date.timeIntervalSince1970)
+    }
+}
+
 class UpdateTests: XCTestCase {
     
     func testJSONMappingWithRequiredFields() {
@@ -22,18 +36,18 @@ class UpdateTests: XCTestCase {
         let firstUpdate = updates[0]
         expect(firstUpdate.bonus) == 10
         expect(firstUpdate.name) == "aaa"
-        expect(firstUpdate.date) == NSDate(timeIntervalSince1970: 1457710841)
+        expect(firstUpdate.date) == NSDate(ISO8601String: "2016-03-11T15:40:41Z")
         expect(firstUpdate.childBonuses.count) == 0
         
         
         let secondUpdate = updates[1]
         expect(secondUpdate.bonus) == 50
         expect(secondUpdate.name) == "bbb"
-        expect(secondUpdate.date) == NSDate(timeIntervalSince1970: 1457710241)
+        expect(secondUpdate.date) == NSDate(ISO8601String: "2016-03-11T15:30:41Z")
         expect(secondUpdate.childBonuses.count) == 1
         expect(secondUpdate.childBonuses[0].bonus) == 20
         expect(secondUpdate.childBonuses[0].name) == "bbb"
-        expect(secondUpdate.childBonuses[0].date) == NSDate(timeIntervalSince1970: 1457711441)
+        expect(secondUpdate.childBonuses[0].date) == NSDate(ISO8601String: "2016-03-11T15:50:41Z")
     }
     
 }
