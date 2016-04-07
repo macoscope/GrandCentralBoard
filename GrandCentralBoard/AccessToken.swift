aca918e475be922ea76c2d23d4fa115ea109c4d6
michallaskowski <michall@macoscope.net>
2016-04-07 12:34:57 +0200

    GCB-128: Add GoogleTokenProvider

---
+++ GrandCentralBoard/AccessToken.swift
@@ -0,0 +1,24 @@
+//
+//  GoogleTokenResponseModel.swift
+//  GrandCentralBoard
+//
+//  Created by Michał Laskowski on 06.04.2016.
+//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//
+
+import Decodable
+
+struct AccessToken : Decodable {
+
+    let token: String
+    let expireDate: NSDate
+
+    init(token: String, expiresIn: Int) {
+        self.token = token
+        self.expireDate = NSDate(timeIntervalSinceNow: NSTimeInterval(expiresIn))
+    }
+
+    static func decode(json: AnyObject) throws -> AccessToken {
+        return try AccessToken(token: json => "access_token", expiresIn: json => "expires_in")
+    }
+}
---
+++ GrandCentralBoard/GoogleTokenProvider.swift
@@ -0,0 +1,53 @@
+//
+//  GoogleTokenProvider.swift
+//  GrandCentralBoard
+//
+//  Created by Michał Laskowski on 06.04.2016.
+//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//
+
+import Alamofire
+
+enum RefreshTokenError : ErrorType {
+    case FailedResponseParsing
+    case UnderlyingError(NSError)
+}
+
+protocol OAuthTokenProvider {
+    func accessTokenFromRefreshToken(completion: (Result<AccessToken, RefreshTokenError>) -> Void)
+}
+
+private let googleRefreshTokenURL = "https://accounts.google.com/o/oauth2/token"
+
+final class GoogleTokenProvider : OAuthTokenProvider {
+
+    private let clientID, clientSecret: String
+    private let refreshToken: String
+    private let refreshURL: URLStringConvertible
+
+    init(clientID: String, clientSecret: String, refreshToken: String, refreshURL: URLStringConvertible = googleRefreshTokenURL) {
+        self.clientID = clientID
+        self.clientSecret = clientSecret
+        self.refreshToken = refreshToken
+        self.refreshURL = refreshURL
+    }
+
+    func accessTokenFromRefreshToken(completion: (Result<AccessToken, RefreshTokenError>) -> Void) {
+        Alamofire.request(.POST, googleRefreshTokenURL, parameters: [
+            "client_id": clientID,
+            "client_secret": clientSecret,
+            "refresh_token": refreshToken,
+            "grant_type": "refresh_token"
+            ]).responseJSON { response in
+                switch response.result {
+                case .Failure(let error): completion(.Failure(.UnderlyingError(error)))
+                case .Success(let json):
+                    if let tokenResponse = try? AccessToken.decode(json) {
+                        completion(.Success(tokenResponse))
+                    } else {
+                        completion(.Failure(.FailedResponseParsing))
+                    }
+                }
+        }
+    }
+}
