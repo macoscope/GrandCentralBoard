1c43a3d53702e131005a5fdf298f48e6c7258409
Rafal Augustyniak <rafal@macoscope.net>
2016-04-12 11:34:44 +0200

    GCB-183: Run tests with xcodebuild

---
+++ scripts/travis-script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+function run_tests
+{
+  xcodebuild test -workspace GrandCentralBoard.xcworkspace -scheme GrandCentralBoard -sdk appletvsimulator9.2 -configuration 'Debug' ONLY_ACTIVE_ARCH=NO | xcpretty -f `xcpretty-travis-formatter`
+}
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+	run_tests || exit $?
+	exit 0
+fi
+
+if [[ "$TRAVIS_BRANCH" == "mvp" ]]; then
+	run_tests || exit $?
+	exit $?
+fi
