ab368c98964ecdeb168319d664aca5f33dbbb7f9
michallaskowski <michall@macoscope.net>
2016-04-18 12:24:29 +0200

    GCB-138: Add GoogleAnalyticsSource and Settings

---
+++ GrandCentralBoard/Widgets/WebsiteAnalytics/GoogleAnalyticsSource.swift
@@ -0,0 +1,39 @@
+//
+//  GoogleAnalyticsSource.swift
+//  GrandCentralBoard
+//
+//  Created by Michał Laskowski on 18.04.2016.
+//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//
+
+import GrandCentralBoardCore
+
+final class GoogleAnalyticsSource : Asynchronous {
+
+    let dataProvider: GoogleAnalyticsDataProvider
+    let interval: NSTimeInterval
+
+    typealias ResultType = Result<[PageViewsRowReport]>
+    var sourceType: SourceType {
+        return .Momentary
+    }
+
+    init(dataProvider: GoogleAnalyticsDataProvider, refreshInterval: NSTimeInterval = 5*60) {
+        self.dataProvider = dataProvider
+        self.interval = refreshInterval
+    }
+
+    func read(closure: (ResultType) -> Void) {
+        let now = NSDate()
+        let fiveDaysAgo = now.dateWith(hour: 0, minute: 0, second: 0)!.dateMinusDays(5)!
+        dataProvider.fetchPageViewsReportFromDate(fiveDaysAgo, toDate: now) { result in
+            switch result {
+            case .Success(let reports):
+                let pageViews = reports.rows.flatMap({ PageViewsRowReport(analyticsReportRow:$0) })
+                return closure(.Success(pageViews))
+            case .Failure(let error):
+                closure(.Failure(error))
+            }
+        }
+    }
+}
---
+++ GrandCentralBoard/Widgets/WebsiteAnalytics/WebsiteAnalyticsSettings.swift
@@ -0,0 +1,30 @@
+//
+//  WebsiteAnalyticsSettings.swift
+//  GrandCentralBoard
+//
+//  Created by Michał Laskowski on 18.04.2016.
+//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//
+
+import Decodable
+
+
+struct WebsiteAnalyticsSettings  {
+    let viewID: String
+    
+    let clientID: String
+    let clientSecret: String
+    let refreshToken: String
+    
+    let refreshInterval: Int
+}
+
+extension WebsiteAnalyticsSettings: Decodable {
+    static func decode(jsonObject: AnyObject) throws -> WebsiteAnalyticsSettings {
+        return try WebsiteAnalyticsSettings(viewID: jsonObject => "viewID",
+                                                clientID: jsonObject => "clientID",
+                                                clientSecret: jsonObject => "clientSecret",
+                                                refreshToken: jsonObject => "refreshToken",
+                                                refreshInterval: jsonObject => "refreshInterval")
+    }
+}
