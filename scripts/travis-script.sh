861df6c1db3d45956020563ad813ac57bfd524c3
Rafal Augustyniak <rafal@macoscope.net>
2016-04-18 16:10:58 +0200

    Use xctool instead of xcodebuild

--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -2,7 +2,7 @@
 
 function run_tests
 {
-  xcodebuild test -workspace GrandCentralBoard.xcworkspace -scheme GrandCentralBoard -sdk appletvsimulator9.2 -configuration 'Debug' ONLY_ACTIVE_ARCH=NO | xcpretty -f `xcpretty-travis-formatter`
+  xcodebuild test -workspace GrandCentralBoard.xcworkspace -scheme GrandCentralBoard -sdk appletvsimulator9.2 -configuration 'Debug' ONLY_ACTIVE_ARCH=NO | xcpretty -c -f `xcpretty-travis-formatter`; exit ${PIPESTATUS[0]}
 }
 
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ "$TRAVIS_BRANCH" == "mvp" ]]; then
