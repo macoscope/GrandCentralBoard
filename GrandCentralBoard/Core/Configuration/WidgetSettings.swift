00ae22fbcb31ed2b0032961875906a91765e4120
Oktawian Chojnacki <oktawian@me.com>
2016-04-21 14:15:34 +0200

    GCB-230: Make `WidgetSettings` hashable

--- GrandCentralBoard/Core/Configuration/WidgetSettings.swift
+++ GrandCentralBoard/Core/Configuration/WidgetSettings.swift
@@ -22,6 +22,36 @@ public struct WidgetSettings {
     }
 }
 
+extension WidgetSettings : Hashable {
+    public var hashValue: Int {
+
+        if let settings = settings as? [String : AnyObject] {
+            return settings.enumerate().reduce(name.hashValue) { first, second in
+
+                if let string = second.element.1 as? String {
+                    return first ^ second.element.0.hashValue ^ string.hashValue
+                }
+
+                if let number = second.element.1 as? NSNumber {
+                    return first ^ second.element.0.hashValue ^ number.hashValue
+                }
+
+                if second.element.1 is NSNull {
+                    return first ^ second.element.0.hashValue
+                }
+
+                return first
+            }
+        }
+
+        return 0
+    }
+}
+
+public func == (lhc: WidgetSettings, rhc: WidgetSettings) -> Bool {
+    return lhc.hashValue == rhc.hashValue
+}
+
 extension WidgetSettings : Decodable {
     public static func decode(jsonObject: AnyObject) throws -> WidgetSettings {
         return try WidgetSettings(name: jsonObject => "name", settings: jsonObject => "settings")
