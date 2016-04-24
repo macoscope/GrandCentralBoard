1a1fc0aa93455921e05a1ce0ce43ad9e6577d972
Oktawian Chojnacki <oktawian@me.com>
2016-04-24 14:54:46 +0200

    GCB-244: Document `Widget`.

--- GrandCentralBoard/Core/Widget.swift
+++ GrandCentralBoard/Core/Widget.swift
@@ -5,19 +5,39 @@
 
 import UIKit
 
+
+/// Class implementing this protocol will have a collection of sources implementing `UpdatingSource` protocol.
 public protocol HavingSources : class {
     var sources: [UpdatingSource] { get }
 }
 
+/// Ability to be updated by calling `update(source:)` method with a source implementing `UpdatingSource` protocol.
 public protocol Updateable : class {
     func update(source: UpdatingSource)
 }
 
+/**
+  Class implementing this protocol have it's controlled `view` and has to implement `Updateable` and `HavingSources` protocols.
+ */
 public protocol WidgetControlling: Updateable, HavingSources {
+    /// This `view` is the de facto Widget being displayed.
     var view: UIView { get }
 }
 
+/// Ability to build a Widget (class implementing `WidgetControlling` protocol) using json data (AnyObject).
 public protocol WidgetBuilding : class {
+
+    /// This is an identifier, used to select proper builder for widget configuration entry.
     var name: String { get }
+
+    /**
+     Actual building method.
+
+     - parameter settings: Deserialized JSON data `AnyObject` (`NSDictionary`).
+
+     - throws: Can throw errors related to deserialization.
+
+     - returns: class implementing `WidgetControlling` protocol
+     */
     func build(settings: AnyObject) throws -> WidgetControlling
 }
