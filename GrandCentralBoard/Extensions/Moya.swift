f6692e8b558c966be8096b22a7dddb01ec86bfa1
michallaskowski <michall@macoscope.net>
2016-05-10 11:26:17 +0200

    GCB-37: Change copyright to 'Macoscope'

--- GrandCentralBoard/Extensions/Moya.swift
+++ GrandCentralBoard/Extensions/Moya.swift
@@ -3,7 +3,7 @@
 //  GrandCentralBoard
 //
 //  Created by Michał Laskowski on 28.04.2016.
-//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//  Copyright © 2016 Macoscope. All rights reserved.
 //
 
 import Moya
