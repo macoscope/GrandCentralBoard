a5288ca8cce1ebf7137f77de3c4d0c503122771a
michallaskowski <michall@macoscope.net>
2016-05-11 13:04:43 +0200

    GCB-287: Add SlackRestAPIProviderTests

--- GrandCentralBoardTests/Widgets/Slack/SlackRestAPIProviderTests.swift
+++ GrandCentralBoardTests/Widgets/Slack/SlackRestAPIProviderTests.swift
@@ -3,7 +3,48 @@
 //  GrandCentralBoard
 //
 //  Created by Michał Laskowski on 11.05.2016.
-//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//  Copyright © 2016 Macoscope. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import Nimble
+import RxSwift
+import OHHTTPStubs
+@testable import GrandCentralBoard
+
+
+final class SlackRestAPIProviderTests: XCTestCase {
+
+    private var urlRequest: NSURLRequest!
+
+    private let rtmStartResponse: [[String: AnyObject]] = {
+        let path = NSBundle(forClass: SlackRestAPIProviderTests.self).pathForResource("repos", ofType: "json")!
+        let jsonData = NSData(contentsOfFile: path)!
+        return try! NSJSONSerialization.JSONObjectWithData(jsonData, options: []) as! [[String: AnyObject]]
+    }()
+
+    private let websocketURL = "wss://some.slack.url/websocket/123"
+
+    override func setUp() {
+        urlRequest = nil
+
+        stub(isPath(SlackTarget.GetWebsocketAddress.path)) { urlRequest in
+            self.urlRequest = urlRequest
+            return OHHTTPStubsResponse(JSONObject: ["ok": true, "url": self.websocketURL], statusCode: 200, headers: nil)
+        }
+    }
+
+    override func tearDown() {
+        OHHTTPStubs.removeAllStubs()
+    }
+
+    func testWebsocketURLFetch() {
+        let provider = SlackRestAPIProvider(accessToken: "")
+        waitUntil { done in
+            let _ = provider.websocketAddress().subscribeNext { url in
+                expect(url.URLString) == self.websocketURL
+                done()
+            }
+        }
+    }
+}
