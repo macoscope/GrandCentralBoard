7d0130443aa2462394231cf4013e7bee5934b99d
michallaskowski <michall@macoscope.net>
2016-05-27 16:17:37 +0200

    Add SlackWidgetView

---
+++ GrandCentralBoard/Widgets/Slack/View/SlackWidgetView.swift
@@ -0,0 +1,17 @@
+//
+//  SlackWidgetView.swift
+//  GrandCentralBoard
+//
+//  Created by Michał Laskowski on 27.05.2016.
+//  Copyright © 2016 Macoscope. All rights reserved.
+//
+
+import UIKit
+
+
+final class SlackWidgetView: UIView {
+
+    @IBOutlet private weak var messageView: MessageBubbleView!
+    @IBOutlet private weak var avatarView: SlackAvatarView!
+
+}
