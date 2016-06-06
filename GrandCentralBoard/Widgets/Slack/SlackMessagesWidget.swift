59e1b596a13e3ac42f7757e098a5ceaaff73a0c3
michallaskowski <michall@macoscope.net>
2016-06-06 17:55:01 +0200

    wip

--- GrandCentralBoard/Widgets/Slack/SlackMessagesWidget.swift
+++ GrandCentralBoard/Widgets/Slack/SlackMessagesWidget.swift
@@ -12,22 +12,23 @@ import RxSwift
 
 final class SlackMessagesWidget: WidgetControlling {
 
-    private let widgetView = SlackWidgetView.fromNib()
+    private let messageView = SlackWidgetView.fromNib()
+    private let widgetView: WidgetTemplateView
 
     let sources: [UpdatingSource]
     private let avatarProvider = AvatarProvider()
     private let disposeBag = DisposeBag()
 
-    let view: UIView
+    var view: UIView { return widgetView }
 
     init(source: SlackSource) {
         sources = [source]
 
         let widgetTemplateViewModel = WidgetTemplateViewModel(title: "Slack".uppercaseString,
-                                                              subtitle: "",
-                                                              contentView: widgetView)
+                                                              subtitle: "No Posts".uppercaseString,
+                                                              contentView: messageView)
         let layoutSettings = WidgetTemplateLayoutSettings(contentMargin: UIEdgeInsets(top: 26, left: 27, bottom: 27, right: 26))
-        view = WidgetTemplateView.viewWithViewModel(widgetTemplateViewModel, layoutSettings: layoutSettings)
+        widgetView = WidgetTemplateView.viewWithViewModel(widgetTemplateViewModel, layoutSettings: layoutSettings)
 
         widgetView.hidden = true
 
@@ -38,8 +39,8 @@ final class SlackMessagesWidget: WidgetControlling {
 
     private func onNewMessage(message: SlackMessage) {
         SlackWidgetViewModel.fromMessage(message, withAvatarProvider: avatarProvider).subscribeNext { [weak self] in
-            self?.widgetView.configureWithViewModel($0)
-            self?.widgetView.hidden = false
+            self?.messageView.configureWithViewModel($0)
+            self?.messageView.hidden = false
         }.addDisposableTo(disposeBag)
     }
 
