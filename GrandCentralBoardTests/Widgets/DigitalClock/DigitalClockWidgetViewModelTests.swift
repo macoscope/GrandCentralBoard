9979a024dcf629b9228956e2666339ca0b334609
Joel Fischer <joeljfischer@gmail.com>
2016-06-06 15:35:35 -0400

    [Digital Clock] Add some tests

---
+++ GrandCentralBoardTests/Widgets/DigitalClock/DigitalClockWidgetViewModelTests.swift
@@ -0,0 +1,50 @@
+//
+//  DigitalClockWidgetViewModelTests.swift
+//  GrandCentralBoard
+//
+//  Created by Joel Fischer on 6/6/16.
+//  Copyright © 2016 Oktawian Chojnacki. All rights reserved.
+//
+
+import XCTest
+import FBSnapshotTestCase
+@testable import GrandCentralBoard
+
+class DigitalClockWidgetViewModelTests: FBSnapshotTestCase {
+
+    private let formatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        formatter.locale = NSLocale(localeIdentifier: "en_US")
+
+        return formatter
+    }()
+
+    let timeZone = NSTimeZone.defaultTimeZone()
+
+    func widgetRenderingViewModel(viewModel: DigitalClockWidgetViewModel) -> UIView {
+        let view = DigitalClockWidgetView.fromNib()
+        view.render(viewModel)
+        return view
+    }
+
+    override func setUp() {
+        super.setUp()
+//        recordMode = true
+    }
+
+    func testClockMidnight() {
+        let date = formatter.dateFromString("2016-06-06 00:00:00")
+
+        let viewModel = DigitalClockWidgetViewModel(date: date!, timeZone: timeZone)
+        FBSnapshotVerifyView(widgetRenderingViewModel(viewModel))
+    }
+
+    func testClock315pm() {
+        let date = formatter.dateFromString("2016-06-06 15:15:15")
+
+        let viewModel = DigitalClockWidgetViewModel(date: date!, timeZone: timeZone)
+        FBSnapshotVerifyView(widgetRenderingViewModel(viewModel))
+    }
+
+}
